ec27ca83e1a74fc98f0600699930115a97adcaef
Efrain Vazquez <efra.chivas.evf@gmail.com>
2021-04-15 23:15:13 -0500

    more improves

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,13 @@ killall polybar -q
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar main -c /home/$USER/.config/polybar/config.ini &
-polybar twomain -c /home/$USER/.config/polybar/config.ini & 
-polybar threemain -c /home/$USER/.config/polybar/config.ini &
+# Launch polybar top's
+polybar top -c /home/$USER/.config/polybar/config.ini &
+polybar twotop -c /home/$USER/.config/polybar/config.ini & 
+polybar threetop -c /home/$USER/.config/polybar/config.ini &
+
+# Launch polybar bottom's
+polybar bottom -c /home/$USER/.config/polybar/config.ini &
+polybar twobottom -c /home/$USER/.config/polybar/config.ini &
+polybar threebottom -c /home/$USER/.config/polybar/config.ini &
+
